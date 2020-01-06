f419f359786bc2341df0e29fc137585f9f56f9c6
abdellah <abdel.ysf@gmail.com>
2020-01-06 14:02:50 +0100

    adding dashboard controller

---
+++ ExamProject/Models/chartMarqueNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamProject.Models
+{
+    public class chartMarqueNumber
+    {
+        public string nomMarque { get; set; }
+        public int count { get; set; }
+    }
+}
